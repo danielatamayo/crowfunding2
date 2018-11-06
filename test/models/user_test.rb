dfe10f944d0a7ab9edf82cb5096720dc43c6a920
Daniela Tamayo <danielatamayo7380@gmail.com>
2018-11-05 23:26:14 -0600

    created first user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,7 +62,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-    test "password should be present (nonblank)" do
+   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
@@ -71,4 +71,6 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  
 end
