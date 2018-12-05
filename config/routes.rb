6631941003e09c7e47cf0da74bd0708ee20ece35
danielatamayo <daniela.tamayo01@utrgv.edu>
2018-12-05 01:09:48 -0600

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,32 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root 'static_pages#home'
-  
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
-
-  get  '/charge',  to: 'static_pages#donate'
-  #get  '/charges',  to: 'static_pages#confirmation'
-  #post '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
 
 
- # post 'users/search' => 'users#search', as: 'search_users'
+  #controller
+  devise_for :users, controllers: { registrations: "registrations" }
 
+  root to: "campaigns#home"
+  get 'dashboard', to: 'campaigns#dashboard'
+  get 'search', to: 'pages#search', :as => 'search_page'
+  get 'terms', to: 'pages#terms'
+  get 'stripe_accounts/full', to: 'stripe_accounts#full'
+  get 'debit_cards/new'
+  post 'debit_cards/create', to: 'debit_cards#create'
+  post 'debit_cards/destroy', to: 'debit_cards#destroy'
+  post 'disputes', to: 'disputes#create'
+  post 'instant_transfer', to: 'debit_cards#transfer'
+  get 'payouts/:id', to: 'payouts#show', as: 'payout'
+  post 'webhooks/stripe', to: 'webhooks#stripe'
+
+
+  #get 'users', to: 'pages#search'
+ 
   resources :users
-  resources :posts,          only: [:create, :destroy]
+
+  resources :campaigns
+
+  resources :stripe_accounts
+
   resources :charges
+
+  resources :bank_accounts
 end
