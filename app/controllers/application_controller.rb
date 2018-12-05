9f04607304a7c57e450b54e411b912809e3acfb2
danielatamayo <daniela.tamayo01@utrgv.edu>
2018-12-04 21:42:40 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include SessionsHelper
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
-  private
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
+  #some error
+  def handle_error(message = "Sorry, something failed.", view = 'new')
+    flash.now[:alert] = message
+    render view
+  end
 end
