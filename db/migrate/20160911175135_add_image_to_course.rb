5b0598f7762017a7383f6f010b7e2f2b9a8adf5f
danielatamayo <daniela.tamayo01@utrgv.edu>
2018-12-04 23:59:37 -0600

    migrate

---
+++ db/migrate/20160911175135_add_image_to_course.rb
@@ -0,0 +1,6 @@
+class AddImageToCourse < ActiveRecord::Migration[5.0]
+  
+  def change
+    add_column :courses, :image, :string
+  end
+end
---
+++ db/migrate/20161228223505_add_active_to_campaign.rb
@@ -0,0 +1,6 @@
+class AddActiveToCampaign < ActiveRecord::Migration[5.0]
+  
+  def change
+    add_column :campaigns, :active, :boolean, default: true
+  end
+end
